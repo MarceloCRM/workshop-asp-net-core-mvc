476b7af8468a4f60bb1fd47c70d8f9173ff36587
MarceloCRM <marcelo.crmoura@gmail.com>
2025-07-07 21:10:04 -0300

    Tratando erros de cascade/restrict no delete

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -16,7 +16,20 @@ namespace SalesWebMvc.Data
 
         public DbSet<Department> Department { get; set; }
         public DbSet<Seller> Seller { get; set; }
-        public DbSet<SalesRecord> SalesRecord { get; set; }
-        
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>()
+            .HasMany(p => p.Sellers)
+            .WithOne(t => t.Department)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Seller>()
+            .HasMany(p => p.Sales)
+            .WithOne(t => t.Seller)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+
     }
 }
